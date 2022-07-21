8fa7da1de3e23d4979c1555da8cfad21c6492a12
PratikPidkalwar <pratikpidkalwar786@gmail.com>
2022-07-21 19:14:29 +0530

    SETEX Used

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -6,28 +6,27 @@ const redis = require("redis");
 const { promisify } = require("util");
 
 
-const isValid = function (value) {
+const isValid = function(value) {
     if (typeof value === "undefined" || value === null || typeof value === 'number') return false;
     if (typeof value === "string" && value.trim().length === 0) return false;
     return true;
 };
 
-const isValidRequestBody = function (requestBody) {
+const isValidRequestBody = function(requestBody) {
     return Object.keys(requestBody).length == 1;
 };
 
 //Connect to redis
 const redisClient = redis.createClient(
     18787,
-   // "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
-    "redis-18787.c301.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
+    // "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    "redis-18787.c301.ap-south-1-1.ec2.cloud.redislabs.com", { no_ready_check: true }
 );
-redisClient.auth("CBouv7eqipQQFOWH0sscDpYsyzRcfLH0", function (err) {
+redisClient.auth("CBouv7eqipQQFOWH0sscDpYsyzRcfLH0", function(err) {
     if (err) throw err;
 });
 
-redisClient.on("connect", async function () {
+redisClient.on("connect", async function() {
     console.log("Connected to Redis..");
 });
 //1. connect to the server
@@ -35,12 +34,12 @@ redisClient.on("connect", async function () {
 
 //Connection setup for redis
 
-const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 
-const createUrl = async function (req, res) {
+const createUrl = async function(req, res) {
     try {
         const requestBody = req.body
 
@@ -56,17 +55,17 @@ const createUrl = async function (req, res) {
         if (!validUrl.isUri(longUrl)) {
             return res.status(400).send({ status: false, messege: 'Invalid  URL' })
         }
-        let cachedUrlData = await GET_ASYNC (`${req.body.longUrl}`)
+        let cachedUrlData = await GET_ASYNC(`${req.body.longUrl}`)
 
-        if(cachedUrlData){
-            return res.status(400).send({status:false,message:"Short link (cache) already generated for this url",data:JSON.parse(cachedUrlData)})
+        if (cachedUrlData) {
+            return res.status(400).send({ status: false, message: "Short link (cache) already generated for this url", data: JSON.parse(cachedUrlData) })
         }
 
         const isAlreadyGen = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
-        // console.log(isAlreadyGen)
+            // console.log(isAlreadyGen)
         if (isAlreadyGen) {
-            await SET_ASYNC(`${req.body.longUrl}`,JSON.stringify(isAlreadyGen))
-            return res.status(400).send({ status:false , message: "Short link already generated for this url", data: isAlreadyGen })
+            await SET_ASYNC(`${req.body.longUrl}`, 10, JSON.stringify(isAlreadyGen))
+            return res.status(400).send({ status: false, message: "Short link already generated for this url", data: isAlreadyGen })
         }
 
         const baseUrl = 'http://localhost:3000'
@@ -88,8 +87,8 @@ const createUrl = async function (req, res) {
             shortUrl: requiredUrl
         }
         let data = await urlModel.create(obj)
-       let createdData = await urlModel.findOne(data).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
-       await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(createdData))
+        let createdData = await urlModel.findOne(data).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
+        await SET_ASYNC(`${req.body.longUrl}`, 10, JSON.stringify(createdData))
         res.status(201).send({ status: true, data: createdData })
 
 
@@ -106,19 +105,18 @@ const createUrl = async function (req, res) {
 
 
 
-const getUrl = async function (req, res) {
+const getUrl = async function(req, res) {
     try {
         const urlCode = req.params.urlCode
         let cachedUrlData = await GET_ASYNC(`${req.params.urlCode}`)
         if (cachedUrlData) {
 
             return res.status(302).redirect(cachedUrlData)
-        }
-        else {
+        } else {
             const url = await urlModel.findOne({ urlCode: urlCode })
             if (url) {
-                  console.log(url)
-           await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(url.longUrl))
+                console.log(url)
+                await SET_ASYNC(`${req.params.urlCode}`, 10, JSON.stringify(url.longUrl))
                 return res.status(302).redirect(url.longUrl)
             } else {
 
@@ -131,7 +129,7 @@ const getUrl = async function (req, res) {
         console.error(err)
         res.status(500).send({ status: false, msg: err.message })
     }
-    }
+}
 
 
 
